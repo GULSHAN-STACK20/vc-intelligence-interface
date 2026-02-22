b2800ab5259efe2e3200d52f9e7d29cc021897ad
Gulshan Kotiya <gulshankotiya20@gmail.com>
2026-02-22 19:10:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import { redirect } from 'next/navigation';
 
-
-<p className="mt-4 text-gray-600">
- Built by Gulshan Kotiya — Full Stack Developer
-</p>
 export default function Home() {
   redirect('/companies');
 }
